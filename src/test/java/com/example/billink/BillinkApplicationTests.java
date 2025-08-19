4868f2ec51a786de8c4aa24744a04124eebc5131
Ricardo55010 <richi_mari.2011@hotmail.com>
2025-08-18 21:39:09 -0600

    start: "New project for personal finance"

---
+++ src/test/java/com/example/billink/BillinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.billink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
