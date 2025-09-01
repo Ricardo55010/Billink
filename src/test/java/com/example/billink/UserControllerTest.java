88a396c3edb3a7fee4ca42560f3920ff58848b21
Ricardo55010 <richi_mari.2011@hotmail.com>
2025-08-31 21:07:24 -0600

    add : profiles for example (needed to define a SPRING_PROFILES_ACTIVE variable

--- src/test/java/com/example/billink/UserControllerTest.java
+++ src/test/java/com/example/billink/UserControllerTest.java
@@ -6,33 +6,23 @@ import com.example.billink.Services.UserService;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
-
-@WebMvcTest(UserController.class)
+import org.springframework.test.context.junit4.SpringRunner;
+@SpringBootTest
 public class UserControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
-    @Mock
-    private UserService userService;
+
     @Test
     public void testGetUser(){
-        try {
-            when(userService.getUser()).thenReturn(new User("Ricardo",1,2));
-            this.mockMvc.perform(get("/")
-                            .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
-                    .andExpect(status().isOk())
-                    .andExpect(content().contentType("application/json"));
-        }catch (Exception e ){
-            System.out.println(e);
-        }
+
         }
 }
