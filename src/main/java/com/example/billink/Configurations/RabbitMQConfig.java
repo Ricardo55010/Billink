3d0df3f4bae1939c93f90798eb9a4204d0096a45
Ricardo55010 <richi_mari.2011@hotmail.com>
2025-09-07 21:28:13 -0600

    edit: added config to read UserDTO produced by the consumer from rabbitmq

--- src/main/java/com/example/billink/Configurations/RabbitMQConfig.java
+++ src/main/java/com/example/billink/Configurations/RabbitMQConfig.java
@@ -1,11 +1,14 @@
 package com.example.billink.Configurations;
 
+import com.example.billink.DTO.UserDTO;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 @Configuration
@@ -27,9 +30,14 @@ public class RabbitMQConfig {
         return BindingBuilder.bind(myQueue).to(myExchange).with("my.routing.key");
     }
 
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
     @RabbitListener(queues = QUEUE_NAME)
-    public void listen(String message) {
-        System.out.println("Received message: " + message);
+    public void listen(UserDTO userDTO) {
+        System.out.println("Received message: " + userDTO);
     }
 
 }
---
+++ src/main/java/com/example/billink/DTO/UserDTO.java
@@ -0,0 +1,49 @@
+package com.example.billink.DTO;
+
+
+public class UserDTO {
+    Long id;
+    String name;
+    Integer age;
+    Integer wage;
+
+    public UserDTO(String name, Integer age, Integer wage) {
+        this.name = name;
+        this.age = age;
+        this.wage = wage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getWage() {
+        return wage;
+    }
+
+    public void setWage(Integer wage) {
+        this.wage = wage;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", wage=" + wage +
+                '}';
+    }
+}
